215c8cd2afc897b0f870810578e808f4386553b4
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-11 14:15:09 -0300

    Change of -executionpolicy from remotesigned to unresctricted.

--- organizador/install.bat
+++ organizador/install.bat
@@ -1,1 +1,1 @@
-Powershell.exe -executionpolicy remotesigned $install = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/organizador/download.ps1;Invoke-Expression $($install.Content)
+Powershell.exe -executionpolicy unrestricted $install = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/organizador/download.ps1;Invoke-Expression $($install.Content)
