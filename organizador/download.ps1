0b665e1494146537ebf6d1daed1fa3c30180b58a
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-11 15:17:01 -0300

    Alteração em pastas e nome de arquivos.

--- organizador/download.ps1
+++ organizador/download.ps1
@@ -1,13 +1,13 @@
 
-$init = "https://raw.githubusercontent.com/bredsan/ESTI/main/organizador/init.bat"
-$init_dest = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$inicializar = "https://raw.githubusercontent.com/bredsan/ESTI/main/organizador/organizador.bat"
+$inicializar_dest = "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 $organizador = "https://raw.githubusercontent.com/bredsan/ESTI/main/organizador/organizador.ps1"
 $organizador_dest = "$home"
 
 
 Import-Module BitsTransfer
-Start-BitsTransfer -Source $init -Destination $init_dest
+Start-BitsTransfer -Source $inicializar -Destination $inicializar_dest
 Start-BitsTransfer -Source $organizador -Destination $organizador_dest
 
 .$home\organizador.ps1
