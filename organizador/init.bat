b17f927945a142d3db9a503975669ffa592f93d6
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-11 14:48:34 -0300

    Proper flow.

--- organizador/init.bat
+++ organizador/init.bat
@@ -1,3 +1,3 @@
-Powershell Unblock-File -Path c:\organizador.ps1
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell c:\users\organizador.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell -Command "Set-ExecutionPolicy Unrestricted"
+Powershell Unblock-File -Path $home\organizador.ps1
+PowerShell .$home\organizador.ps1
