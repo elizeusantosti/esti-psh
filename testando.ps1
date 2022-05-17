1160ae2cc2db973abd1cd619a3dcc6afc7a17f03
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-17 17:26:53 -0300

    Add files via upload

---
+++ testando.ps1
@@ -0,0 +1,41 @@
+﻿$desktop = get-item $home\Desktop\*
+
+$extensoes = "\.(lnk)","\.(exe)","\.(txt|xls|xlsx|doc|docx|pdf)"
+
+$i=0
+
+$categoria = @($null)
+
+foreach ($arquivo in $desktop) {foreach ($extensao in $extensoes) {if ($arquivo -match $extensao) {$categoria[$i] = $desktop -match $extensao}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# FUNCIONANDO
+<#
+$atalhos = $origem -match "\.(lnk)"
+
+$executaveis = $origem -match "\.(exe)"
+
+$documentos = $origem -match "\.(txt|xls|xlsx|doc|docx|pdf)"
+
+
+$lista = $atalhos,$executaveis,$documentos
+
+
+$i=0
+
+
+foreach ($item in $lista) {if ($item.Count -ne 0) {$categoria[$i++] = $item} else {"NOT"}}
+#>
