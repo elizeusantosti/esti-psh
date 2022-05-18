486c0cf639ef9a52322b224a8e65ac82bf84f79a
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-18 20:07:26 -0300

    Update scoop_check_install.ps1

--- automatizador/scoop_check_install.ps1
+++ automatizador/scoop_check_install.ps1
@@ -1,8 +1,6 @@
 $origem = get-item $home\*
 
-$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/esti_padrao.ps1;Invoke-Expression $($esti_padrao.Content)
-$install_scoop = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/install_scoop.ps1;Invoke-Expression $($install_scoop.Content)
+$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/esti_padrao.ps1
+$install_scoop = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/install_scoop.ps1
 
-
-
-if ($origem -match "scoop") {$esti_padrao.ps1} else {$install_scoop}
+if ($origem -match "scoop") {Invoke-Expression $($esti_padrao.Content)} else {Invoke-Expression $($install_scoop.Content)}
