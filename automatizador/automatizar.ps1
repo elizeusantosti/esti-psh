ee970520b7ba537cd4d3c44713095e593ac27d5e
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-18 11:14:21 -0300

    Update automatizar.ps1

--- automatizador/automatizar.ps1
+++ automatizador/automatizar.ps1
@@ -2,3 +2,6 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iwr -useb get.scoop.sh | iex
 scoop install git
 scoop bucket add extras
+scoop install wpsoffice
+scoop install chrome
+scoop install java
