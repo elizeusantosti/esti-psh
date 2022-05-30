36611fc919db1effeef3de6dc97fc37822b0d45c
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-05-30 17:39:19 -0300

    Update esti.ps1

--- esti.ps1
+++ esti.ps1
@@ -1,8 +1,10 @@
 # Criar alias esti
-# esti | Exibe a p·gina de ajuda do comando.
-# esti help | Exibe a p·gina de ajuda.
-# esti update | Checa a vers„o, atualiza o cÛdigo fonte e faz download dos demais scripts em sua ˙ltima vers„o se necess·rio.
+# esti | Exibe a p√°gina de ajuda do comando.
+# esti help | Exibe a p√°gina de ajuda.
+# esti update | Checa a vers√£o, atualiza o c√≥digo fonte e faz download dos demais scripts em sua √∫ltima vers√£o se necess√°rio.
 
+
+# J√° funciona com multiplos comandos, ex: esti update organizar automatizar backup 
 
 function esti {
     foreach ($arg in $args) {
