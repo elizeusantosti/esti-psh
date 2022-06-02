2bdad1b7b542053db89b4e854d88b12e5ab96a5f
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-02 18:12:40 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,8 @@
-# Importa o módulo de download de arquivos.
+# Importa o mÃ³dulo de download de arquivos.
 Import-Module BitsTransfer
 
-$perfil = "$home\documents\windowspowershell\Microsoft.PowerShell_profile.ps1"
+$perfil = "Microsoft.powershell_profile.ps1"
+$perfil_pasta = "$home\documents\windowspowershell\"
 
 # Define scripts a serem baixados.
 $scripts = "organizar","backup","arquivos"
@@ -12,15 +13,12 @@ $destino = "$home\documents\windowspowershell\esti"
 # Verifica se existe a pasta de destino e baixa os arquivos para a pasta.
 md $destino -ErrorAction SilentlyContinue
 
-# Função para baixar os scripts e enviar para a pasta de destino.
+# FunÃ§Ã£o para baixar os scripts e enviar para a pasta de destino.
 foreach ($script in $scripts){Start-BitsTransfer -Source "https://raw.githubusercontent.com/bredsan/ESTI/main/$script.ps1" -Destination $destino}
 
 
 
 # Cria os aliases.
-cd $env:USERPROFILE\Documents
-md WindowsPowerShell -ErrorAction SilentlyContinue
-cd WindowsPowerShell
-New-Item Microsoft.PowerShell_profile.ps1 -ItemType "file" -ErrorAction SilentlyContinue
-curl -o $perfil "https://raw.githubusercontent.com/bredsan/ESTI/main/alias.ps1"
+md $perfil_pasta -ErrorAction SilentlyContinue
+curl -o $perfil "https://raw.githubusercontent.com/bredsan/esti/main/alias.ps1"
 $profile
