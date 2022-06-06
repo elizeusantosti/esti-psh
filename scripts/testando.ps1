8e6012b9d43f446c07498dccab0dbe849773fe97
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-06 16:27:19 -0300

    ok

--- scripts/testando.ps1
+++ scripts/testando.ps1
@@ -1,3 +1,48 @@
+$origem = "$home\desktop"
+$arquivos  = get-childitem $origem
+
+$categorias = @(("Atalhos","\.(lnk)"),
+("Executaveis","\.(exe)"),
+("Documentos\Texto","\.(txt)"),
+("Documentos\PDF","\.(pdf)"),
+("Documentos\Excel","\.(xls)"),
+("Documentos\Word","\.(doc)"),
+("Documentos\Powerpoint","\.(exe)")
+)
+
+foreach ($item in $categorias) {
+    $categoria = $item[0]
+    $extensao = $item[1]
+    if ($arquivos -match $extensao) {
+        $arquivo_atual = $arquivos -match $extensao
+        $arquivo_atual
+        $destino=$origem,$categoria -Join "\"
+        if (test-path $destino) {
+            Move-Item $arquivo_atual -destination $destino
+        }
+        else
+        {
+            md $destino
+            Move-Item $arquivo_atual -destination $destino
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<#
 $arquivos = get-childitem -File $home\Desktop\
 
 function extensao {
@@ -8,3 +53,4 @@ function extensao {
 }
 
 extensao txt doc xls pdf
+#>
