08be218854cf140fb1f0858482762f1bb4056767
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-06 17:08:24 -0300

    Extensoes adicionadas

---
+++ scripts/organizar_testing.ps1
@@ -0,0 +1,42 @@
+$origem = "$home\desktop"
+$arquivos = get-childitem $origem
+$categorias = @(
+("\.(lnk)","$origem\Atalhos"),
+("\.(exe)","$origem\Executaveis"),
+("\.(txt)","$origem\Documentos\Texto"),
+("\.(pdf)","$origem\Documentos\PDF"),
+("\.(xls)","$origem\Documentos\Excel"),
+("\.(doc)","$origem\Documentos\Word"),
+("\.(pptx)","$origem\Documentos\Powerpoint"),
+("\.(jpg)","$origem\Imagens\JPG"),
+("\.(png)","$origem\Imagens\PNG"),
+("\.(gif)","$origem\Imagens\GIF"),
+("\.(mp3)","$origem\Audio\MP3"),
+("\.(wav)","$origem\Audio\WAV"),
+("\.(aiff)","$origem\Audio\AIFF"),
+("\.(mp4)","$origem\Video\MP4"),
+("\.(3gp)","$origem\Video\3GP"),
+("\.(mkv)","$origem\Video\MKV"),
+("\.(html)","$origem\Desenvolvimento\WEB\HTML"),
+("\.(css)","$origem\Desenvolvimento\WEB\CSS"),
+("\.(js)","$origem\Desenvolvimento\WEB\JavaScript"),
+("\.(py)","$origem\Desenvolvimento\SCRIPT\Python"),
+("\.(ps1)","$origem\Desenvolvimento\SCRIPT\Powershell"),
+("\.(psm)","$origem\Desenvolvimento\SCRIPT\Powershell")
+)
+
+
+foreach ($item in $categorias){
+    $extensao = $item[0]
+    $destino = $item[1]
+    if ($arquivos -match $extensao){
+        $categoria_atual = $arquivos -match $extensao
+    }
+    if (test-path $destino){
+        move-item $categoria_atual -Destination $destino
+    }
+    else{
+        md $destino
+        move-item $categoria_atual -Destination $destino
+    }
+}
