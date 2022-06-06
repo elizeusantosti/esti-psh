c95ce018d5286b369e75c36b1a1f567f2eca6e40
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-06 16:29:32 -0300

    testing

---
+++ scripts/test.ps1
@@ -0,0 +1,10 @@
+$origem = "$home\desktop"
+
+$arquivos = get-childitem $origem
+
+$categoria = "\.(lnk)","\.(exe)","\.(txt)"
+
+foreach ($item in $categoria){
+    $categoria_atual = $arquivos -match $item
+    $categoria_atual
+}
