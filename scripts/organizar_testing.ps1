fa634d0543b09aaf040118f0620f8bf486604a4b
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-06 17:20:32 -0300

    Correção Destino2

--- scripts/organizar_testing.ps1
+++ scripts/organizar_testing.ps1
@@ -34,8 +34,8 @@ foreach ($item in $categorias){
     $categoria_atual = $arquivos -match $extensao
 
     if (test-path $destino){
-    move-item $categoria_atual $destino}
+    move-item $categoria_atual -destination $destino}
     else {
     md $destino
-    move-item $categoria_atual $destino}
+    move-item $categoria_atual -destination $destino}
 }
