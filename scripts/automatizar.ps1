47b9ddec7c80ad194f5fdb1ebb759a95d48922d0
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-07 18:43:38 -0300

    Update automatizar.ps1

--- scripts/automatizar.ps1
+++ scripts/automatizar.ps1
@@ -1,6 +1,6 @@
 $origem = get-item $home\*
 
-$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/esti_padrao.ps1
-$install_scoop = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/ESTI/main/automatizador/install_scoop.ps1
+$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/esti/main/automatizar/esti_padrao.ps1
+$scoop_padrao = Invoke-WebRequest https://raw.githubusercontent.com/bredsan/esti/main/automatizar/scoop_padrao.ps1
 
-if ($origem -match "scoop") {Invoke-Expression $($esti_padrao.Content)} else {Invoke-Expression $($install_scoop.Content)}
+if ($origem -match "scoop") {Invoke-Expression $($esti_padrao.Content)} else {Invoke-Expression $($scoop_padrao.Content)}
