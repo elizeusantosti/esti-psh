b7c2ff507df4d9582e2e6e5b00ab7fbd70abdd01
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-07 17:39:56 -0300

    clean code

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -3,6 +3,5 @@ function backup {
     $origem = $args[1]
     $destino = "X:\"
     $exclusao = @("*appdata*", "*programdata*")
-    get-childitem -path $origem -destination $destino -exclude $exclusao -recurse 
-    
+    get-childitem -path $origem -destination $destino -exclude $exclusao -recurse
 }
