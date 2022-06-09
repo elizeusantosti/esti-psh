a4c8860f9951c0177991f9cad7d3c6926710b2f0
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-09 18:02:57 -0300

    Update check.ps1

--- scripts/check.ps1
+++ scripts/check.ps1
@@ -5,7 +5,9 @@ Faz a checagem dos scripts e suas variaveis na pasta esti
 
 Fazer checagem de processos por meio de invocação de scripts.
 
-Exemplo: Ao rodar esti organizar
+Exemplo: Ao rodar esti auto-install checa se já foi feita a instalação anteriormente.
+
+
 
 #>
 
