493b4542d8a36fd1b3fa03d930c9c3534947068d
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-09 11:37:42 -0300

    scoop test ok

--- scripts/automatizar.ps1
+++ scripts/automatizar.ps1
@@ -1,6 +1,6 @@
 $origem = get-item $home\*
 
-$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/automatizador/esti_padrao.ps1
-$scoop_padrao = Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/automatizador/scoop_padrao.ps1
+$esti_padrao = Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/scripts/automatizar/esti_padrao.ps1
+$scoop_padrao = Invoke-WebRequest hhttps://raw.githubusercontent.com/elizeusantosti/esti/main/scripts/automatizar/scoop_padrao.ps1
 
 if ($origem -match "scoop") {Invoke-Expression $($esti_padrao.Content)} else {Invoke-Expression $($scoop_padrao.Content)}
