b7dd81e3235c74fa5be86062405d7b72085233a8
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-10 14:39:10 -0300

    esti refactor test

--- esti.ps1
+++ esti.ps1
@@ -1,1 +1,9 @@
-"Ambiente em desenvolvimento"
+$parametros = "backup","auto-install"
+
+$parametro = foreach ($item in $parametros)
+{
+    $posicao = [array]::LastIndexOf($args,"$item")+1
+    if($posicao -ne 0){$args[$posicao]}else{$null}        
+}
+
+$parametro
