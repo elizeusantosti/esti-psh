619c44247e2dca40b451455e3ec5cad5d269c95f
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-10 14:41:36 -0300

    test

--- esti.ps1
+++ esti.ps1
@@ -3,7 +3,6 @@ $parametros = "backup","auto-install"
 $parametro = foreach ($item in $parametros)
 {
     $posicao = [array]::LastIndexOf($args,"$item")+1
-    if($posicao -ne 0){$args[$posicao]}else{$null}        
+    if($posicao -ne 0){$args[$posicao]}else{$null}
+    $item      
 }
-
-$parametro
