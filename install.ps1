ba8be602d9788944d4b783930572b8b95f501661
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-12 20:06:11 -0300

    add: esti core, install

---
+++ install.ps1
@@ -0,0 +1,15 @@
+﻿# Define a função para importar arquivos do github. 
+function import([string]$arquivo){
+    Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/$arquivo | Invoke-Expression
+}
+
+# Importa o core do programa.
+import esti_core
+
+# Se não existir cria a respectiva pasta de destino.
+nova_pasta scripts $pasta_esti
+
+
+foreach ($script in $scripts){
+    download $script $scripts_url $pasta_esti
+}
