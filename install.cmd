bba67c22a5a7ccd0ff6379f07be0e7b485cb83ef
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-12 20:02:03 -0300

    refactoring main

--- install.cmd
+++
@@ -1,1 +0,0 @@
-Powershell.exe -executionpolicy unrestricted $install = Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/install/install.ps1;Invoke-Expression $($install.Content)
