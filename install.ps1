5e32709f8f79acaa4bc0cc58181e0c1939690ad5
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-12 21:40:31 -0300

    pasta_esti corrigida

--- install.ps1
+++ install.ps1
@@ -16,9 +16,9 @@ function download([string]$arquivo, [string]$origem, [string]$destino) {
     }
 }
 
-function nova_pasta($pasta, $destino) {
+function nova_pasta($pasta) {
     Try {
-        New-Item -Path $destino\$pasta -ItemType Directory -ErrorAction Stop
+        New-Item -Path $pasta -ItemType Directory -ErrorAction Stop
     }
     Catch {
         Write-Host $_.Exception.Message -ForegroundColor Red
@@ -29,9 +29,9 @@ $scripts = "automatizar.ps1","arquivos.ps1","organizar.ps1","backup.ps1"
 
 $scripts_url =  "https://raw.githubusercontent.com/elizeusantosti/esti/main/scripts/"
 
-$pasta_esti = "$home\documents\windowspowershell\esti\"
+$pasta_esti = "$home\documents\windowspowershell\esti\scripts"
 
-nova_pasta scripts $pasta_esti
+nova_pasta $pasta_esti
 
 foreach ($script in $scripts){
     download $script $scripts_url $pasta_esti
