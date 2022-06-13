ea0a97f23705663f2a55329efef787f0782397f0
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-12 22:16:20 -0300

    test importing core fcuntions

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
+Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/core.ps1 | Invoke-Expression
+
 nova_pasta  $pasta_esti
 
 foreach ($script in $scripts){
-    donwload $script $scripts_url $pasta_esti
+    download $script $scripts_url $pasta_esti
 }
