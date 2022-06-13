a44ac853080708268322b2003c3041b9eeb337bf
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-13 12:01:31 -0300

    error

--- profile.ps1
+++ profile.ps1
@@ -1,13 +1,4 @@
 $caminho = split-path -parent $MyInvocation.MyCommand.Definition
-
-function backup{
-    [[CmdletBinding()]
-    param (
-        [Parameter()]
-        [TypeName]
-        $origem
-    )]
-}
 function esti {
     foreach ($arg in $args) {
         & "$caminho\esti\scripts\$arg.ps1"
