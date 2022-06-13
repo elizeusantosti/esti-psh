71e4babff9ef59df8096f7ae61a4c2f77fed21a6
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-12 22:13:07 -0300

    corrigindo erros na instalação

---
+++ core.ps1
@@ -0,0 +1,32 @@
+function import($arquivo){
+    try {
+        Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/$arquivo | Invoke-Expression
+    }
+    catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
+}
+
+function download([string]$arquivo, [string]$origem, [string]$destino) {
+    Try {
+        Start-BitsTransfer -Source $origem/$arquivo -Destination $destino
+    }
+    Catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
+}
+
+function nova_pasta($pasta) {
+    Try {
+        New-Item -Path $pasta -ItemType Directory -ErrorAction Stop
+    }
+    Catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
+}
+
+$scripts = "automatizar.ps1","arquivos.ps1","organizar.ps1","backup.ps1"
+
+$scripts_url =  "https://raw.githubusercontent.com/elizeusantosti/esti/main/scripts/"
+
+$pasta_esti = "$home\documents\windowspowershell\esti\scripts\"
--- install.ps1
+++ install.ps1
@@ -1,41 +1,5 @@
-﻿function invoke([string]$arquivo){
-    try {
-        Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/$arquivo | Invoke-Expression
-    }
-    catch {
-        Write-Host $_.Exception.Message -ForegroundColor Red
-    } 
-}
-
-function download([string]$arquivo, [string]$origem, [string]$destino) {
-    Try {
-        Start-BitsTransfer -Source $origem/$arquivo -Destination $destino
-    }
-    Catch {
-        Write-Host $_.Exception.Message -ForegroundColor Red
-    }
-}
-
-function nova_pasta($pasta) {
-    Try {
-        New-Item -Path $pasta -ItemType Directory -ErrorAction Stop
-    }
-    Catch {
-        Write-Host $_.Exception.Message -ForegroundColor Red
-    }
-}
-
-$scripts = "automatizar.ps1","arquivos.ps1","organizar.ps1","backup.ps1"
-
-$scripts_url =  "https://raw.githubusercontent.com/elizeusantosti/esti/main/scripts/"
-
-$pasta_esti = "$home\documents\windowspowershell\esti\scripts\"
-
-
-# Executar passos
-
-nova_pasta $pasta_esti
+nova_pasta  $pasta_esti
 
 foreach ($script in $scripts){
-    download $script $scripts_url $pasta_esti
+    donwload $script $scripts_url $pasta_esti
 }
