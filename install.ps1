0902d85624e7632be8d75e8e1363011699f52a51
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-13 01:01:19 -0300

    correção pasta principal

--- install.ps1
+++ install.ps1
@@ -6,4 +6,4 @@ foreach ($script in $scripts){
     download $script $main_scripts $pasta_esti_scripts
 }
 
-download Microsoft.Powershell_profile.ps1 $pasta_powershell
+download Microsoft.Powershell_profile.ps1 $pasta_principal
