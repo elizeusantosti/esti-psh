584b42235a4de2b697a29ffba5aeea1955c5c478
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-14 18:29:48 -0300

    Create padrao.ps1

---
+++ padrao.ps1
@@ -0,0 +1,4 @@
+$main =  "https://raw.githubusercontent.com/elizeusantosti/esti/main" 
+$powershell = "$home\documents\windowspowershell" 
+$modules = "$powershell\modules" 
+$pasta_scripts = "$powershell\scripts" 
