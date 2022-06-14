62a6d2b7ca8f478d3d250d2fcf34ac8746a1a498
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-14 12:17:16 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,11 +1,12 @@
+# Invoca as funcoes necessarias para a instalacao dos modulos.
 Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/esti_functions.psm1 | Invoke-Expression
-Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis.ps1 | Invoke-Expression
 
+# Se nao existir cria as seguintes pastas. 
 nova-pasta  $modules\esti
 nova-pasta  $modules\esti_functions
 nova-pasta  $pasta_scripts\esti
 
-
+# Baixa os arquiovs necessarios para o funcionamento do modulo.
 baixar $main/esti.psm1 $modules\esti
 baixar $main/variaveis.ps1 $modules\esti
 baixar $main/esti_functions.psm1 $modules\esti_functions
