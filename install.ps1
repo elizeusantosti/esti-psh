851bd6289e64505d77de5667c4ff527c7e02b170
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-14 18:35:29 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -7,13 +7,15 @@ Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/est
 
 
 # Se nao existir cria as seguintes pastas. 
-nova-pasta  $modules\esti
-nova-pasta  $modules\esti_functions
-nova-pasta  $pasta_scripts\esti
+nova-pasta $modules\esti
+nova-pasta $modules\esti_functions
+nova-pasta $pasta_scripts\esti
+nova-pasta $pasta_scripts\variaveis
 
 # Baixa os arquivos necessarios para o funcionamento do modulo.
 baixar $main/esti.psm1 $modules\esti
-baixar $main/variaveis.ps1 $modules\esti
+baixar $main/variaveis/padrao.ps1 $pasta_scripts\variaveis
+baixar $main/variaveis/apps.ps1 $pasta_scripts\variaveis
 baixar $main/esti_functions.psm1 $modules\esti_functions
 $scripts = "automatizar.ps1","arquivos.ps1","organizar.ps1","backup.ps1"
 foreach ($script in $scripts) {baixar $main/scripts/$script $pasta_scripts\esti}
