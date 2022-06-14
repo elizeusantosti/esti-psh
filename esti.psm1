0012714a974d5cdb714be8758da58230ae47f0b4
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-14 14:02:03 -0300

    Função Desinstalar adicionada.

--- esti.psm1
+++ esti.psm1
@@ -9,3 +9,17 @@ function esti{
         Write-Host $_.Exception.Message -ForegroundColor red
     }
 }
+
+function desinstalar{
+    param(
+        [CmdletBinding()][Parameter(mandatory, position=0)]$modulo
+    )
+    try {
+        rm -r -fo $modules\$modulo
+        rm -r -fo $modules\"$modulo"_functions
+        rm -r -fo $pasta_scripts\$modulo
+    }
+    catch {
+        Write-Host $_.Exception.Message -ForegroundColor red
+    }
+}
