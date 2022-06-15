08b237cba6c5f34dc97c7f9abd27e5760917a2c4
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-15 20:03:12 -0300

    Correção ao invocar modulo esti_functions

--- install.ps1
+++ install.ps1
@@ -1,7 +1,8 @@
 # Invoca as funcoes necessarias.
-Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/esti_functions.psm1 | Invoke-Expression
+Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/modulos/esti_functions.psm1 | Invoke-Expression
 
-Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1 | Invoke-Expression
+#Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1 | Invoke-Expression
+importar padrao -remoto
 
 # Se nao existir cria as seguintes pastas.
 $pasta.variaveis.values | foreach {criar-pasta $_}
