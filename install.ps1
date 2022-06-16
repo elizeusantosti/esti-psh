5736db17bf9515064b89e690236fcf5cc404faa8
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-16 00:46:48 -0300

    Correção de invocação variaveis

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 # Invoca as funcoes necessarias.
-Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/modulos/esti_functions.psm1 | Invoke-Expression
-Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1 | Invoke-Expression
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/esti_functions.ps1" | Invoke-Expression
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1" | Invoke-Expression
 
 # Se nao existir cria as seguintes pastas.
 $pasta.variaveis.values | foreach {criar-pasta $_}
