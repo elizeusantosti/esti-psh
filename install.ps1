0db69dddffe1ef5d3eb505475b10b54339ec52c0
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-15 22:46:28 -0300

    ok

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/modulos/esti_functions.psm1 | Invoke-Expression
 
 #Invoke-WebRequest https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1 | Invoke-Expression
-importar padrao -remoto
+importar padrao remoto
 
 # Se nao existir cria as seguintes pastas.
 $pasta.variaveis.values | foreach {criar-pasta $_}
