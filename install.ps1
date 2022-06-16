d1b43f1829cf2859a2456592cc5a1b7655db3ddb
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-16 00:56:30 -0300

    Correção 2 linha

--- install.ps1
+++ install.ps1
@@ -3,12 +3,12 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/ma
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1" | Invoke-Expression
 
 # Se nao existir cria as seguintes pastas.
-$pasta.variaveis.values | foreach {criar-pasta $_}
+criar-pasta $pasta.variaveis.values
 $pasta.modules.values | foreach {criar-pasta $_}
 $pasta.scripts.values | foreach {criar-pasta $_}
 
 # Baixa os arquivos necessarios para o funcionamento do modulo.
-$main.variaveis.values | foreach {baixar $_ $pasta.variaveis}
+$main.variaveis.values | foreach {baixar $_ $pasta.variaveis.esti}
 
 baixar $main.modulos.esti $pasta.modules.esti
 baixar $main.modulos.esti_functions $pasta.modules.esti_functions
