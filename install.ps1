a65179ea84b4af51c94b24ff32ca1ceb28c4b773
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-06-17 16:31:52 -0300

    Corrigido

--- install.ps1
+++ install.ps1
@@ -1,26 +1,22 @@
-# Invoca as funcoes necessarias.
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/modulos/esti_functions.psm1" | Invoke-Expression
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1" | Invoke-Expression
+# Carrega as variáveis
+$padrao = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/elizeusantosti/esti/main/variaveis/padrao.ps1"
+# É preciso invocar a url 2 vezes, como se estivesse em cache.
+Invoke-Expression $padrao
+Invoke-Expression $padrao
 
-$pasta.variaveis.values
-$pasta.modules.values
-$pasta.scripts.values
-$main.variaveis.values
-$main.modulos.esti
-$main.modulos.esti_functions
-$main.scripts.values
+# Carrega as funções
+$functions = Invoke-WebRequest -Uri $main.modulos.esti_functions
+Invoke-Expression $functions
 
-<#
 # Se nao existir cria as seguintes pastas.
 criar-pasta $pasta.variaveis.values
-$pasta.modules.values | foreach {criar-pasta $_}
-$pasta.scripts.values | foreach {criar-pasta $_}
+$pasta.modules.values | ForEach-Object {criar-pasta $_}
+$pasta.scripts.values | ForEach-Object {criar-pasta $_}
 
 # Baixa os arquivos necessarios para o funcionamento do modulo.
-$main.variaveis.values | foreach {baixar $_ $pasta.variaveis.esti}
+$main.variaveis.values | ForEach-Object {baixar $_ $pasta.variaveis.esti}
 
 baixar $main.modulos.esti $pasta.modules.esti
 baixar $main.modulos.esti_functions $pasta.modules.esti_functions
 
-$main.scripts.values | foreach {baixar $_ $pasta.scripts.esti}
-#>
+$main.scripts.values | ForEach-Object {baixar $_ $pasta.scripts.esti}
