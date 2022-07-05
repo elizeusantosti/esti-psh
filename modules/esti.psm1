652cdc376373a991a207bf397795d4a7548ba9d2
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-07-05 15:48:47 -0300

    ok

--- modules/esti.psm1
+++ modules/esti.psm1
@@ -1,4 +1,4 @@
-Invoke-Expression -Command '.$home\documents\windowspowershell\database\esti\variaveis.ps1'
+Invoke-Expression -Command '.$home\documents\windowspowershell\core\esti\database.ps1'
 
 function esti(){
     [cmdletbinding()]
@@ -6,48 +6,11 @@ function esti(){
         [Parameter(Mandatory=$true, Position=0)]
         [string]$script
     )
-    $secret_scripts = $pasta.scripts.esti+"$script.ps1"
+    $secret_scripts = $pasta.scripts+"$script.ps1"
     try {
         Invoke-Expression -Command '.$secret_scripts'
     }
     catch {
         Write-Host $_.Exception.Message -ForegroundColor Red
-    }
-}
-
-# Estrutura OK
-function backup{
-    [cmdletbinding()]
-    param(
-        [Parameter(position=0)]
-        [string]$origem
-    ),
-    [cmdletbinding()]
-    param(
-        [Parameter(position=0)]
-        [string]$destino
-    )
-    switch($item){
-        'origem' {}
-        'destino'{}
-    }
-    
-}
-
-function baixar{
-    [CmdLetBinding()]
-    Param(
-        [Parameter(position = 0)]
-        [ValidateSet('app','arquivo')]
-        [string]$item,
-        [Parameter(mandatory = $true, position = 1)]
-        [string]$nome,
-        [Parameter(position = 2)]
-        [string]$destino
-
-    )
-    Switch ($item){
-        'app' {Invoke-WebRequest -url $instaladores[$nome] $destino}
-        'arquivo'{Invoke-WebRequest -url $url[$nome] $destino}
     }
 }
