be7901e7093ce12cfea1cadef15cdf63accec74b
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-07-08 12:43:17 -0300

    testando

--- scripts/instaladores.ps1
+++ scripts/instaladores.ps1
@@ -1,12 +1,15 @@
 ﻿# Define a branch principal.
 $global:branch = "testing"
 
+$destino = "c:\util"
+
 # Importa os arquivos de configuração do script.
 iwr -uri "https://raw.githubusercontent.com/elizeusantosti/esti/$branch/core/functions.ps1" | iex
 iwr -uri "https://raw.githubusercontent.com/elizeusantosti/esti/$branch/core/instaladores.ps1" | iex
 
 # Baixa os instaladores.
 $instaladores.GetEnumerator() | foreach {
-    baixar $_.value "$home\downloads"
+    baixar $_.value $destino
     $app = $_.key; Write-Host("O $app está sendo baixado...")
+    # Testar > $destino Get-Items | foreach {.$_}
 }
