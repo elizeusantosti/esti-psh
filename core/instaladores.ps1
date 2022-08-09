ae347dbc4f32f3a2218e5666331ec7ff2306ea13
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-08-09 12:37:30 -0300

    Update instaladores.ps1

--- core/instaladores.ps1
+++ core/instaladores.ps1
@@ -1,7 +1,7 @@
 $global:instaladores = @{
     wub = "https://www.sordum.org/files/download/windows-update-blocker/Wub.zip"
     zip  = "https://www.7-zip.org/a/7z2201.exe"
-    dwservice = "https://www.dwservice.net/download/dwagent_x86.exe"
+    dwservice = "https://node172222.dwservice.net/getAgentFile.dw?name=dwagent_x86.exe"
     java = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246805_424b9da4b48848379167015dcc250d8d"
     driver_booster = "https://cdn.iobit.com/dl/driver_booster_setup.exe"
     chrome = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BA35772B8-AE1C-8A0F-E502-360F37295EFC%7D%26lang%3Dpt-BR%26browser%3D5%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dstable-arch_x86-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup.exe"
