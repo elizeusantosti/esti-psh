2ac9577b2a3332840d2ce9972516e4fb2d8a90ea
Breno Sant'ana <53102923+bredsan@users.noreply.github.com>
2022-08-09 10:56:46 -0300

    att x86 apps

--- core/instaladores.ps1
+++ core/instaladores.ps1
@@ -1,9 +1,9 @@
 $global:instaladores = @{
     wub = "https://www.sordum.org/files/download/windows-update-blocker/Wub.zip"
-    zip  = "https://www.7-zip.org/a/7z2200-x64.exe"
+    zip  = "https://www.7-zip.org/a/7z2201.exe"
     dwservice = "https://www.dwservice.net/download/dwagent_x86.exe"
-    java = "https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u333-b02/2dee051a5d0647d5be72a7c0abff270e/jre-8u333-windows-x64.exe?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u333-b02/2dee051a5d0647d5be72a7c0abff270e/jre-8u333-windows-x64.exe&BHost=javadl.sun.com&File=jre-8u333-windows-x64.exe&AuthParam=1657039548_f9944d25bf07560263f9df10721a58af&ext=.exe"
+    java = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246805_424b9da4b48848379167015dcc250d8d"
     driver_booster = "https://cdn.iobit.com/dl/driver_booster_setup.exe"
-    chrome = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B54E90C0E-367E-00CC-C089-3904F63FCAB0%7D%26lang%3Dpt-BR%26browser%3D5%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe"
+    chrome = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BA35772B8-AE1C-8A0F-E502-360F37295EFC%7D%26lang%3Dpt-BR%26browser%3D5%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dstable-arch_x86-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup.exe"
     wps = "https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/onlinesetup/distsrc/600.1006/wpsinst/wps_office_inst.exe"
 }
